60c0824a328aa0343ac2728fbd4e4b5ec57a4ca2
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-07-29 10:23:17 +0100

    chores: update emailVerifcation component

--- src/app/Components/emailVerification/EmailVerification.tsx
+++ src/app/Components/emailVerification/EmailVerification.tsx
@@ -12,7 +12,7 @@ const EmailVerification: React.FC<EmailVerificationProps> = ({ email, onResend, 
       setAnnouncement('Verification email resent');
     }
   };
-
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg text-center">
